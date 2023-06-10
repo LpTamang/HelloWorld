a8cc999ca386192f6f70d675163f44da7352bea7
lakpatamang <lakpatamang12@gmail.com>
2023-06-09 20:35:38 -0400

    Successfully Completed Hello World

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World?");
+        System.out.println("K Cha?");
+        System.out.println("Kooding Ko Kam Cha?");
+        System.out.println("Hello Java");
+        System.out.println("Hello Lp?");
+        System.out.println("Hello Prakash?");
+        System.out.println("Hello Anish?");
+        System.out.println("Hello Chase?");
+    }
+}
